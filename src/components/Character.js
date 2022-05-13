4c9f4ebc0066bb8b0560ff18ab166f2a01015e20
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-13 14:19:54 +0100

    styles cards

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 const Character = ({ character }) => {
   return (
-    <div className='character'>
-      <h2>{character.name}</h2>
-      <img src={character.image} className='avatar'></img>
+    <div className='character-card'>
+      <div className='character-card-inner'>
+        <div className='card-front'>
+          <img src={character.image} className='avatar'></img>
+        </div>
+        <div className='card-back'>
+          <h2>{character.name}</h2>
+          <ul>
+            <li>Stats: {character.status}</li>
+            <li>Species: {character.species}</li>
+            <li>Gender: {character.gender}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
