df672bfaafdf635e7efdfba6b92392a827f5dc54
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-14 23:08:59 +0100

    adds refresh to logo name

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -4,7 +4,10 @@ import Search from './Search';
 const TitleBar = ({ onFormSubmit }) => {
   return (
     <div>
-      <img src='logo.png' className='logo' alt='rick and morty name'></img>
+      <a href='http://localhost:3000/'>
+        <img src='logo.png' className='logo' alt='rick and morty name'></img>
+      </a>
+
       <h1 id='title'>Chararters Grid</h1>
       <Search onFormSubmit={onFormSubmit} />
     </div>
